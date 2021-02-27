38222a5a3fe64a05b1959bb05b82d909af3902f0
Zach <53745970+zach7815@users.noreply.github.com>
2021-02-27 18:22:06 +0000

    Main Commit
    Loading of all working files for miniproject

---
+++ README.txt
@@ -0,0 +1,5 @@
+Project inspired by JS Begginners. 
+https://jsbeginners.com/hex-change-background-color-project/
+
+My Version using all my own styling and code. 
+Additionally, my version adds additional funtionality and a bigger array of colors to change from. 
---
+++ index.js
@@ -0,0 +1,27 @@
+const button = document.querySelector(".btn");
+const title = document.querySelector(".title");
+var body = document.querySelector("body");
+
+
+function randomColor()  {
+    let color ="#";
+    let letters = "0123456789ABCDEF";
+    for ( i=0; i<6; i++){
+        color += letters[Math.floor(Math.random()*16)];
+    }
+    return color
+}
+
+button.addEventListener("click", ()=>{
+    body.style.backgroundColor= randomColor();
+  })
+
+  button.addEventListener("click", ()=>{
+      title.innerText= "Hex-Color:" + randomColor()
+  })
+
+  body.addEventListener("keydown", (e)=>{
+      if(e.control==="space"){
+          body.style.backgroundColor=randomColor()
+      }
+  })
